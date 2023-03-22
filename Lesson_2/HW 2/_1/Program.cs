8405b464489aa33933bfd78909e48cfefed41f80
Halima <l-ahim@mail.ru>
2023-03-22 16:42:17 +0300

    HW 2\_1

---
+++ Lesson_2/HW 2/_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1. Напишите программу, которая принимает на вход трехзначное число и на 
+//    выходе показывает вторую цифру этого числа.
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return num / 10 % 10;
+}
+
+int th_num = new Random().Next(100, 1000);
+int result = TakeNum(th_num);
+
+Console.WriteLine(result);
