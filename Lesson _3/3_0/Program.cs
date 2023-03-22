cbe7b480bf98d165fb8c12662cf1b3f33290bf4f
Halima <l-ahim@mail.ru>
2023-03-22 22:43:51 +0300

    task_3_0

--- Lesson _3/3_0/Program.cs
+++ Lesson _3/3_0/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Демо. 1. Напишите программу, которая принимает на вход координаты
+//       точки (Х и Y), причем Х =/= 0 или Y =/= 0 и выдает номер четверти 
+//       плоскости, в которой находится эта точка.
+
+void Quarters(int x, int y)
+{
+    if (x == 0 || y == 0)
+        Console.WriteLine("x = 0, y = 0");
+    else if (x > 0 && y > 0)
+        Console.WriteLine("I");
+    else if (x < 0 && y > 0)
+        Console.WriteLine("II");
+    else if (x < 0 && y < 0)
+        Console.WriteLine("III");
+    else if (x > 0 && y < 0)
+        Console.WriteLine("IV");
+}
+
+int num_1 = int.Parse(Console.ReadLine()!);
+int num_2 = int.Parse(Console.ReadLine()!);
+Quarters(num_1, num_2);
